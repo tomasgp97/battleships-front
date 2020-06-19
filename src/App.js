8ac1b3dc5a76d5317059dda27eeb020c3c8d2308
rodrigo <ropa1998@gmail.com>
2020-06-19 20:24:23 -0300

    Updated login with MUI

--- src/App.js
+++ src/App.js
@@ -6,25 +6,51 @@ import Lobby from "./components/Lobby";
 import Setup from "./components/Setup";
 import Game from "./components/Game";
 import socketIOClient from "socket.io-client";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import {ThemeProvider} from '@material-ui/styles';
 
 const ENDPOINT = "http://127.0.0.1:5000";
 const socket = socketIOClient(ENDPOINT);
 
 function App() {
+
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: 'dark',
+        },
+    });
+
+
     return (
         <>
-            <div className="App">
-                <Router>
-                    <div className="container">
-                        <Switch>
-                            <Route exact path='/' render={(props) => <LoginByGoogle {...props} socket={socket} />}/>
-                            <Route path='/lobby' render={(props) => <Lobby {...props} socket={socket} />}/>
-                            <Route path='/setup' render={(props) => <Setup {...props} socket={socket} />}/>
-                            <Route path='/game' render={(props) => <Game  {...props}socket={socket} />}/>
-                        </Switch>
-                    </div>
-                </Router>
-            </div>
+            <ThemeProvider theme={darkTheme}>
+                <div className="App">
+                    <Router>
+                        <div className="container">
+                            <Switch>
+                                <Route exact path='/' render={(props) => <LoginByGoogle {...props} socket={socket}/>}/>
+                                <Route path='/lobby' render={(props) => <Lobby {...props} socket={socket}/>}/>
+                                <Route path='/setup' render={(props) => <Setup {...props} socket={socket}/>}/>
+                                <Route path='/game' render={(props) => <Game  {...props} socket={socket}/>}/>
+                            </Switch>
+                        </div>
+                    </Router>
+                </div>
+            </ThemeProvider>
+
         </>
     );
 }
--- src/components/LoginByGoogle.js
+++ src/components/LoginByGoogle.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react'
-
+import React from 'react'
 import GoogleLogin from 'react-google-login';
-
-import {Redirect} from 'react-router-dom';
-
+import {useHistory} from "react-router-dom";
 import axios from 'axios'
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-export class LoginByGoogle extends Component {
+export default function LoginByGoogle() {
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+    const history = useHistory()
 
-    signup(res) {
+    function signup(res) {
         const googleresponse = {
             googleId: res.profileObj.googleId,
             email: res.profileObj.email,
@@ -23,43 +22,55 @@ export class LoginByGoogle extends Component {
             .then((result) => {
                 console.log(result)
                 sessionStorage.setItem("userData", JSON.stringify(googleresponse));
-                this.props.history.push('/lobby')
+                history.push('/lobby')
             });
-    };
-
-    render() {
-        const responseGoogle = (response) => {
-            console.log(response);
-            var res = response.profileObj;
-            console.log(res);
-            this.signup(response);
-        }
-
-        return (
-            <div className="App">
-                <div style={{'paddingTop': "20px"}} className="col-sm-12">
-                    <GoogleLogin
-                        clientId="277387142817-veuud45dkct39ivh7qmjq2v8muf3ju85.apps.googleusercontent.com"
-                        buttonText="Login with Google"
-                        cookiePolicy={'single_host_origin'}
-                        onSuccess={(e) => responseGoogle(e)}
-                        onAutoLoadFinished={() => console.log("Auto load Finished")}
-                        onFailure={(e) => console.log(e)}/>
-                </div>
-            </div>
-        )
     }
+
+    function responseGoogle(response) {
+        console.log(response);
+        var res = response.profileObj;
+        console.log(res);
+        signup(response);
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            marginTop: theme.spacing(10),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: "space-around",
+        },
+        avatar: {
+            margin: theme.spacing(3),
+        },
+    }));
+
+    const styles = useStyles()
+
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={styles.paper}>
+                <Avatar className={styles.avatar}>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <GoogleLogin
+                    clientId="277387142817-veuud45dkct39ivh7qmjq2v8muf3ju85.apps.googleusercontent.com"
+                    buttonText="Login with Google"
+                    cookiePolicy={'single_host_origin'}
+                    onSuccess={(e) => responseGoogle(e)}
+                    onAutoLoadFinished={() => console.log("Auto load Finished")}
+                    onFailure={(e) => console.log(e)}/>
+            </div>
+        </Container>
+    )
 }
 
 
-const sendToken = (e) => {
-    axios.post("http://localhost:5000/register", {token: e.tokenId}).then((data) => {
-        console.log("Token sent");
-        console.log(data);
-    }).catch((err) => {
-        console.log("Token not sent");
-        console.log(err);
-    });
-};
 
-export default LoginByGoogle
+
+
